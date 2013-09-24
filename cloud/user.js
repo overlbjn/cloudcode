36c9380f1b34f929045ad182952f769658a5c8a2
overlbjn <402309558@qq.com>
2013-09-24 21:02:57 +0800

    Update user.js

--- cloud/user.js
+++ cloud/user.js
@@ -128,45 +128,33 @@ AV.Cloud.define('testCloopen', function(request, response)
 
     //+ authorization64 + sig + body
     //注册云通信
-    AV.Cloud.httpRequest({
-  url: 'http://www.google.com/search',
-  params: {
-    q : 'Sean Plott'
-  },
-  success: function(httpResponse) {
-    console.log(httpResponse.text);
-  },
-  error: function(httpResponse) {
-    console.error('Request failed with response code ' + httpResponse.status);
-  }
-});
 
-    AV.Cloud.httpRequest({
-        method: 'POST',
-        url: 'https://app.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase(),
-        headers: {
-            'Content-Type': 'application/xml',
-            'Accept': 'application/xml',
-            'charset': 'utf-8',
-            'Authorization': authorization64
-        },
-        body: {
-            SubAccount:{
-                appId:'aaf98f894032b2370140479684b0009f',
-                friendlyName:'123456@qq.com',
-                accountSid:'aaf98f894032b237014047963bb9009d'
+    // AV.Cloud.httpRequest({
+    //     method: 'POST',
+    //     url: 'https://app.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase(),
+    //     headers: {
+    //         'Content-Type': 'application/xml',
+    //         'Accept': 'application/xml',
+    //         'charset': 'utf-8',
+    //         'Authorization': authorization64
+    //     },
+    //     body: {
+    //         SubAccount:{
+    //             appId:'aaf98f894032b2370140479684b0009f',
+    //             friendlyName:'123456@qq.com',
+    //             accountSid:'aaf98f894032b237014047963bb9009d'
                 
-            }
-        },
-        success: function(httpResponse) {
-            // console.log(httpResponse.text);
-            response.success(httpResponse.text);
-        },
-        error: function(httpResponse) {
-            // console.error('Request failed with response code ' + httpResponse.status);
-            respose.error('Request failed with response code ' + httpResponse.status);
-        }
-    });
+    //         }
+    //     },
+    //     success: function(httpResponse) {
+    //         // console.log(httpResponse.text);
+    //         response.success(httpResponse.text);
+    //     },
+    //     error: function(httpResponse) {
+    //         // console.error('Request failed with response code ' + httpResponse.status);
+    //         respose.error('Request failed with response code ' + httpResponse.status);
+    //     }
+    // });
 
 });
 
