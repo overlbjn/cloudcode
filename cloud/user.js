e9164c090d527403a9189107234713b9c3b0003d
overlbjn <402309558@qq.com>
2013-09-24 21:01:29 +0800

    Update user.js

--- cloud/user.js
+++ cloud/user.js
@@ -129,6 +129,19 @@ AV.Cloud.define('testCloopen', function(request, response)
     //+ authorization64 + sig + body
     //注册云通信
     AV.Cloud.httpRequest({
+  url: 'http://www.google.com/search',
+  params: {
+    q : 'Sean Plott'
+  },
+  success: function(httpResponse) {
+    console.log(httpResponse.text);
+  },
+  error: function(httpResponse) {
+    console.error('Request failed with response code ' + httpResponse.status);
+  }
+});
+
+    AV.Cloud.httpRequest({
         method: 'POST',
         url: 'https://app.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase(),
         headers: {
