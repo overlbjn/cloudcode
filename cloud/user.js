06050a71b81662ee556c92eb644ca9308b1245ec
overlbjn <402309558@qq.com>
2013-09-25 01:18:26 +0800

    Update user.js

--- cloud/user.js
+++ cloud/user.js
@@ -5,87 +5,7 @@ var crypto = require('crypto');
 var moment = require('moment');
 var Buffer = require('buffer').Buffer;
 
-//注册
-AV.Cloud.define('register', function(request, response)
-{
 
-    //fadfasdf
-    var username = request.params.username;
-    var password = request.params.password;
-    var email = request.params.email;
-
-    if (username && password && email){
-
-
-        var user = new AV.User();
-        user.set("username", username);
-        user.set("password", password);
-        user.set("email", email);
-
-
-
-        user.signUp(null, {
-            success: function(user) {
-                response.success(user);
-            },
-            error: function(user, error) {
-                response.error(user, error);
-            }
-        });
-
-    }
-});
-
-
-//登录
-AV.Cloud.define('login', function(request, response)
-{
-
-    var username = request.params.username;
-    var password = request.params.password;
-    var theLatitude = request.params.latitude;
-    var theLongitude = request.params.longitude;
-    var place = request.params.place;
-
-    AV.User.logIn(username, password,
-        {
-            success: function(user)
-            {
-
-                if (latitude && longitude && place)
-                {
-                    var userGeoPoint = new AV.GeoPoint({latitude: theLatitude, longitude: theLongitude});
-                    user.set("location",userGeoPoint);
-                    user.save(null, {
-                        success: function(user)
-                        {
-                            response.success(user,user.get(location));
-                        },
-                        error: function(user, error)
-                        {
-                            response.success(user,user.get(location),error);
-                        }
-                    });
-                }
-                else
-                {
-                    response.success(user,user.get(location));
-                }
-            },
-            error: function(user, error) {
-                'asd'.toUpperCase();
-                response.error(user, error);
-
-            }
-        });
-});
-
-//exports.md5 = function (str) {
-//    var md5sum = crypto.createHash('md5');
-//    md5sum.update(str);
-//    str = md5sum.digest('hex');
-//    return str;
-//};
 
 function md5 (text) {
 
@@ -96,22 +16,8 @@ function base64 (text){
     return new Buffer("text").toString('base64');
 }
 
-AV.Cloud.define('md5Test', function(request, response)
-{
-    response.success(md5('HEHE'));
-});
-
-AV.Cloud.define('base64Test', function(request, response)
-{
-
-    response.success(new Buffer("HEHEHE").toString('base64'));
-});
-
 AV.Cloud.define('testCloopen', function(request, response)
 {
-//    var d = new Date();
-//    var timeStr = d.getFullYear().toString + (d.getMonth()+1).toString + d.getDate().toString + d.getHours().toString + d.getMinutes().toString + d.getSeconds().toString;
-
     var timeStr = moment().format('YYYMMDDHHmmss');
 
     var authorizationStr = 'aaf98f894032b237014047963bb9009d'+':'+timeStr;
@@ -122,51 +28,27 @@ AV.Cloud.define('testCloopen', function(request, response)
 
     var sig = md5(sigstr);
     
-    // response.success('ai');
     var bodyxml ='<SubAccount><appId>aaf98f894032b2370140479684b0009f</appId><friendlyName>1234556@qq.com</friendlyName><accountSid>aaf98f894032b237014047963bb9009d</accountSid></SubAccount>';
 
     // response.success(sig.toUpperCase());
 
-    //+ authorization64 + sig + body
-    //注册云通信
-    // AV.Cloud.httpRequest({
-    //     method: 'POST',
-    //     url:'http://www.google.com',
-    //     // url: 'https://app.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase(),
-    //     // headers: {
-    //         // 'Content-Type': 'application/xml',
-    //         // 'Accept': 'application/xml',
-    //         // 'charset': 'utf-8',
-    //         // 'Authorization': authorization64
-    //     // },
-    //     // body: bodyxml,
-    //     body:{
-    //         params:'q=Sean Plott'
-    //     }
-    //     success:function(httpResponse) {
-    //         // console.log(httpResponse.text);
-    //         response.success(httpResponse.text);
-    //     },
-    //     error:function(httpResponse) {
-    //         // console.error('Request failed with response code ' + httpResponse.status);
-    //         respose.error('Request failed with response code ' + httpResponse.status);
-    //     }
-    // });
     AV.Cloud.httpRequest({
-  method: 'POST',
-  url: 'http://www.google.com',
-  body: {
-    params:'q=Sean Plott'
-  },
-  success: function(httpResponse) {
-    // console.log(httpResponse.text);
-    response.success(httpResponse.text);
-  },
-  error: function(httpResponse) {
-    // console.error('Request failed with response code ' + httpResponse.status);
-    respose.error('Request failed with response code ' + httpResponse.status);
-  }
-});
-
+        method: 'POST',
+        url: 'https://app.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase(),
+        headers: {
+            'Content-Type': 'application/xml',
+            'Accept': 'application/xml',
+            'charset': 'utf-8',
+            'Authorization': authorization64
+        },
+        body: bodyxml,
+        success:function(httpResponse) {
+            response.success(httpResponse.text);
+        },
+        error:function(httpResponse) {
+            respose.error('Request failed with response code ' + httpResponse.status);
+        }
+    });
+    
 });
 
