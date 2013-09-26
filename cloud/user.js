34928d80538f3478e19685de391ea6e27de61a39
overlbjn <402309558@qq.com>
2013-09-26 16:33:36 +0800

    Update user.js

--- cloud/user.js
+++ cloud/user.js
@@ -12,6 +12,9 @@ function md5 (text) {
     return crypto.createHash('md5').update(text).digest('hex');
 };
 
+function base64 (text){
+    return new Buffer(text).toString('base64');
+}
 
 AV.Cloud.define('testCloopen', function(request, response)
 {
@@ -19,24 +22,23 @@ AV.Cloud.define('testCloopen', function(request, response)
     console.log('timestr:' + timeStr);
     var authorizationStr = 'aaf98f894032b237014047963bb9009d'+':'+timeStr;
     console.log('authorizationStr:' + authorizationStr);
-    var authorization64 = new Buffer(authorizationStr).toString('base64');
+    var authorization64 = base64(authorizationStr);
     console.log('authorization64:' + authorization64);
     var sigstr = 'aaf98f894032b237014047963bb9009d'+'bbc381b9a024443da462307cec93ce0b'+timeStr;
     console.log('sigstr:' + sigstr);
     var sig = md5(sigstr);
     console.log('sig:' + sig);
     var bodyxml ='<SubAccount><appId>aaf98f894032b2370140482ac6dc00a8</appId><friendlyName>33dd556@qq.com</friendlyName><accountSid>aaf98f894032b237014047963bb9009d</accountSid></SubAccount>';
-    console.log('body:' + bodyxml);
-    console.log('url:https://app.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase());
+    
     // response.success('body:'+bodyxml);
     // response.success('https://sandboxapp.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase()),
     AV.Cloud.httpRequest({
         method: 'POST',
-        url: 'https://sandboxapp.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase(),
+        url: 'https://app.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase(),
         headers: {
             'Content-Type': 'application/xml;charset=utf-8',
             'Accept': 'application/xml',
-            'Authorization': new Buffer(authorizationStr).toString('base64')
+            'Authorization': authorization64
         },
         body: bodyxml,
         success:function(httpResponse) {
@@ -50,4 +52,3 @@ AV.Cloud.define('testCloopen', function(request, response)
     });
     
 });
-
