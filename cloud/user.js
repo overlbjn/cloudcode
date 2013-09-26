ce3b441f4615c09411f21fc31c0bf27c940fbabf
overlbjn <402309558@qq.com>
2013-09-26 11:31:48 +0800

    Update user.js

--- cloud/user.js
+++ cloud/user.js
@@ -41,13 +41,11 @@ AV.Cloud.define('testCloopen', function(request, response)
             'Authorization': authorization64
         },
         body: bodyxml,
-        
         success:function(httpResponse) {
             response.success(httpResponse.text);
         },
         error:function(httpResponse) {
-            response.success('body:'+bodyxml+'rul:'+url+'headers:'+headers);
-            response.error('Request failed with response code ' + httpResponse.status);
+            response.error('Request failed with response code ' + httpResponse.status+'body:'+bodyxml+'rul:'+url+'headers:'+headers);
         }
     });
     
